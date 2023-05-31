e4b4b99ca526d432b82adc7db819eef2d9f887a2
Wolcken_Git <Alfredoxd00@hotmail.com>
2023-05-30 23:00:49 -0400

    Add Drawer Navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, Text, View } from 'react-native';
 import { AppProvider } from './context/AppContext';
+import { DrawerItemList, createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
 import AppBar from './components/AppBar';
@@ -12,17 +15,154 @@ import Sheds from './screens/Sheds';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
 import Welcome from './screens/Welcome';
+import CustomColors from './stylus/colors';
 
 //icons
-//import { Ionicons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
+//Drawers navigation
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+    return (
+        <Drawer.Navigator
+            drawerContent={
+                (props) => {
+                    return (
+                        <SafeAreaView>
+                            <View style={{ 
+                                height: 200,
+                                width: '100%',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                borderBottomColor: '#f4f4f4',
+                                borderBottomWidth: 1,
+                                // backgroundColor: 'blue'
+                             }}>
+                                <Image
+                                    source={require('./assets/images/pirates.png')}
+                                    style={{ 
+                                        height: 130,
+                                        width: 130,
+                                        borderRadius: 65,
+                                        backgroundColor: CustomColors.primary
+                                     }}
+                                />
+                                <Text style={{ 
+                                    fontSize: 20,
+                                    marginVertical: 6,
+                                    fontWeight: 'bold',
+                                    color: CustomColors.primary,
+                                 }}>
+                                    Alfredo Ramos
+                                </Text>
+                                <Text style={{ 
+                                    fontSize: 16,
+                                    color: CustomColors.secondary
+                                 }}>
+                                    Administrador
+                                </Text>
+                            </View>
+                            <DrawerItemList {...props} />
+                        </SafeAreaView>
+                    )
+                }
+            }
+            initialRouteName='WelcomeD'
+            screenOptions={{
+                drawerStyle: {
+                    backgroundColor: CustomColors.white,
+                    width: 200
+                },
+                headerStyle: {
+                    backgroundColor: CustomColors.secondary
+                },
+                headerTintColor: CustomColors.white,
+                headerTitleStyle: {
+                    fontWeight: 'bold'
+                },
+                drawerActiveTintColor: CustomColors.primary,
+                drawerLabelStyle: {
+                    color: CustomColors.primary
+                }
+            }}
+        >
+            <Drawer.Screen
+                name="WelcomeD"
+                component={Welcome}
+                options={{
+                    drawerLabel: 'Home',
+                    title: 'CocaRoad',
+                    drawerIcon: () => (
+                        <Ionicons name='home' size={24} color={CustomColors.primary} />
+                    )
+                }}
+            />
+            <Drawer.Screen
+                name="Adepcoca"
+                component={Home}
+                options={{
+                    drawerLabel: 'Adepcoca',
+                    title: 'Adepcoca',
+                    drawerIcon: () => (
+                        <FontAwesome5 name="money-bill-wave" size={20} color={CustomColors.primary} />)
+                }}
+            />
+            <Drawer.Screen
+                name="Sacaba"
+                component={Home}
+                options={{
+                    drawerLabel: 'Sacaba',
+                    title: 'Sacaba',
+                    drawerIcon: () => (
+                        <FontAwesome5 name="money-bill-wave" size={20} color={CustomColors.primary} />)
+                }}
+            />
+            <Drawer.Screen
+                name="Minoristas"
+                component={Home}
+                options={{
+                    drawerLabel: 'Minoristas',
+                    title: 'Minoristas',
+                    drawerIcon: () => (
+                        <FontAwesome5 name="money-bill-wave" size={20} color={CustomColors.primary} />
+                    )
+                }}
+            />
+            <Drawer.Screen
+                name="Login"
+                component={Login}
+                options={{
+                    drawerLabel: 'Login',
+                    title: 'Login',
+                    drawerIcon: () => (
+                        <Entypo name="login" size={24} color={CustomColors.primary} />
+                    )
+                }}
+            />
+        </Drawer.Navigator>
+    );
+}
+
+
+//Stacks Navigation
 const Stack = createNativeStackNavigator();
 
-function MyStacks() {
+function MyStack() {
     return (
         <Stack.Navigator
             initialRouteName='Welcome'
         >
+            <Stack.Screen
+                name='Welcome'
+                component={MyDrawer}
+                options={{
+                    headerShown: false
+                }}
+            />
             <Stack.Screen
                 name='Home'
                 component={Home}
@@ -33,13 +173,6 @@ function MyStacks() {
             <Stack.Screen
                 name='Sheds'
                 component={Sheds}
-                options={{
-                    headerShown: false
-                }}
-            />
-            <Stack.Screen
-                name='Welcome'
-                component={Welcome}
                 options={{
                     headerShown: false
                 }}
@@ -62,6 +195,8 @@ function MyStacks() {
     )
 }
 
+
+//Tabs Navigation
 // const Tab = createBottomTabNavigator();
 
 // function MyTabs() {
@@ -77,7 +212,7 @@ function MyStacks() {
 //         >
 //             <Tab.Screen
 //                 name='Home'
-//                 component={MyStacks}
+//                 component={MyStack}
 //                 options={{
 //                     title: "Inicio",
 //                     headerShown: false,
@@ -88,9 +223,9 @@ function MyStacks() {
 //             />
 //             <Tab.Screen
 //                 name='Sheds'
-//                 component={Sheds}
+//                 component={Login}
 //                 options={{
-//                     tabBarLabel: 'Precios',
+//                     tabBarLabel: 'Login',
 //                     headerShown: false,
 //                     tabBarIcon: ({ color, size }) => (
 //                         <Ionicons name="add-circle" size={size} color={color} />
@@ -105,9 +240,10 @@ export default function Navigation() {
     return (
         <AppProvider>
             <NavigationContainer>
-                <AppBar />
+                {/* <AppBar /> */}
+                {/* <MyDrawer /> */}
                 {/* <MyTabs /> */}
-                <MyStacks />
+                <MyStack />
             </NavigationContainer>
         </AppProvider>
     )
